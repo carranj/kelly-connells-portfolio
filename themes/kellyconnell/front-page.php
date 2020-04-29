1f74f31876929239f1ddc8606d96401a48cac251
carranj <carranco.jose.r@gmail.com>
2020-04-29 02:06:33 -0500

    update homepage to be editable

--- themes/kellyconnell/front-page.php
+++ themes/kellyconnell/front-page.php
@@ -3,12 +3,12 @@
     get_sidebar();
     $image = get_field('featured_image'); 
 ?>
-<div <?php post_class('container p-0'); ?>>
+<div <?php post_class('container-fluid p-0'); ?>>
     <div class="homelanding" style="background-image:url(<?php echo esc_url($image['url']) ?>); background-size:cover;">
         <div class="home-content text-center">
-            <h2>Educator</h2>
-            <h2>Creative</h2>
-            <h2>Band Director</h2>
+            <h2><?php the_field('featured_line_1'); ?></h2>
+            <h2><?php the_field('featured_line_2'); ?></h2>
+            <h2><?php the_field('featured_line_3'); ?></h2>
             <a href="#" title="Play video" class="play"></a>
         </div>
     </div>
